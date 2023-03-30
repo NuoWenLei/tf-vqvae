665373dc75863ff14af338156ea1c047baecfb7a
NuoWenLei <nuowen0612@gmail.com>
2023-03-30 04:28:31 -0400

    use inferred value, which is so wack

--- model.py
+++ model.py
@@ -112,7 +112,7 @@ def get_image_vqvae(latent_dim=EMBEDDING_DIM, num_embeddings=NUM_EMBEDDINGS, num
 	inputs = tf.keras.Input(shape=(IMAGE_HEIGHT, IMAGE_WIDTH, num_channels))
 	encoder_outputs = encoder(inputs)
 	encoder_shape = tf.shape(encoder_outputs)[1:]
-	decoder = get_decoder(encoder_shape, latent_dim = latent_dim)
+	decoder = get_decoder(encoder_shape._inferred_value, latent_dim = latent_dim)
 	quantized_latents = vq_layer(encoder_outputs)
 	reconstructions = decoder(encoder_shape, quantized_latents)
 	return tf.keras.Model(inputs, reconstructions, name=name)
