b3557a10f011203abedc876f8ff0cbb949a24210
NuoWenLei <nuowen0612@gmail.com>
2023-05-16 20:23:44 -0700

    update minor things

--- download_dataset.py
+++ download_dataset.py
@@ -1,8 +1,8 @@
 from helpers.dataset_helpers import download_dataset_to_local_path
 from helpers.constants import LOCAL_DATASET_PATH
 
-def main():
+def download():
 	download_dataset_to_local_path(dataset_name="sbu_captions", disk_dir=LOCAL_DATASET_PATH)
 
 if __name__ == "__main__":
-    main()
+    download()
